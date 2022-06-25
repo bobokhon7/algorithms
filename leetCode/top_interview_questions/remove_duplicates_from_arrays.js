4a1f935e11f787f8b07da29192d94da2c9d0b64f
Bobokhon <rboboxon6055@gmail.com>
2022-06-25 18:33:58 +0900

    top inteview question 1st and 2nd questions solved

---
+++ leetCode/top_interview_questions/remove_duplicates_from_arrays.js
@@ -0,0 +1,17 @@
+var removeDuplicates = function (nums) {
+  let counter = 1;
+  let first = 0;
+  let second = 0;
+  while (first < nums.length - 1) {
+    if (nums[first] !== nums[first + 1]) {
+      nums[counter] = nums[first + 1];
+      second = first;
+      counter = counter + 1;
+    }
+    first = first + 1;
+  }
+
+  return counter;
+};
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
