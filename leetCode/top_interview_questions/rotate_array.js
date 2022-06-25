22823602553c8acdaeb8d8ae6fc24728af382a2c
Bobokhon <rboboxon6055@gmail.com>
2022-06-25 23:16:15 +0900

    rotate array live coding done 🚀

---
+++ leetCode/top_interview_questions/rotate_array.js
@@ -0,0 +1,28 @@
+var rotate = function (nums, k) {
+  const result = [];
+  const array_l = nums.length;
+
+  for (let i = 0; i < array_l; i++) {
+    let new_index = (i + k) % array_l;
+    result[new_index] = nums[i];
+  }
+
+  for (let i = 0; i < array_l; i++) {
+    nums[i] = result[i];
+  }
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
+// [1, 2, 3, 4, 5, 6, 7]
+// [1, 2, 3, 4, 5, 6, 7]
+// [5, 6, 7, 1, 2, 3, 4]
+
+// 1: 0 -> 4
+// 5: 4 -> 0   // (old_index + k) % nums.length
+// 6: 5 -> 1
+// 7: 6 -> 2
+//    9 -> 2
+
+// ----------- new explanation ---
+// [1, 2, 3, 4, 1, 2, 3]
+// [4, 5, 6, 1, 2, 3, 7]
