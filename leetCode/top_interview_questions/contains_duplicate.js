0b8da7f8c716224a4807bfa9c819e936770371a7
Bobokhon <rboboxon6055@gmail.com>
2022-07-03 12:06:21 +0900

    single number  optimized solution

--- leetCode/top_interview_questions/contains_duplicate.js
+++ leetCode/top_interview_questions/contains_duplicate.js
@@ -1,12 +1,33 @@
 var containsDuplicate = function (nums) {
   let newValue = nums.sort((a, b) => a - b);
 
-  for (let i = 0; i < newValue.length; i++) {
-    if (newValue[i - 1] === newValue[i]) {
+  for (let i = 0; i < newValue.length - 1; i++) {
+    if (newValue[i] === newValue[i + 1]) {
       return true;
     }
   }
   return false;
 };
 
-console.log(containsDuplicate([3, 3]));
+/// Space compelxity O(n)
+/// Time complexity  O(n)
+
+var _containsDuplicate = function (nums) {
+  const set_array = new Set(nums);
+
+  return nums.length === set_array.size;
+};
+
+console.log(_containsDuplicate([1, 2, 3, 4]));
+
+///[1,2,2,3,3,4]
+// 1===2 => false
+// 2===2 => true
+
+//// [1,2,3,3,]
+// 1===2 => false
+// 2===3 => false
+// 3 === 3 => true
+
+/////  [3,3]
+// 3 === 3 => true
--- leetCode/top_interview_questions/single_number.js
+++ leetCode/top_interview_questions/single_number.js
@@ -1,3 +1,25 @@
+////////
+// bitwise operator
+
+// AND // && // true if both values are true, otherwise false
+// OR  // || // true if one of the both values are true, false if both are false
+
+// XOR // if both values are same, result is 0 (false) and if they are not same, the result will be true
+
+var _singleNumber = function (nums) {
+  let result = 0;
+
+  nums.forEach((item) => {
+    result = result ^ item;
+    console.log(result);
+  });
+
+  return result;
+};
+
+console.log(_singleNumber([4, 1, 2, 1, 2]));
+
+//////////
 var singleNumber = function (nums) {
   let newValue = nums.sort((a, b) => a - b);
 
@@ -8,7 +30,10 @@ var singleNumber = function (nums) {
   }
 };
 
-console.log(singleNumber([4, 1, 2, 1, 2]));
+/// Space compelxity O(n)
+/// Time complexity  O(n)
+
+// console.log(singleNumber([4, 1, 2, 1, 2]));
 
 //// [1,1,2,2,4] => i +=2 so increased double
 
